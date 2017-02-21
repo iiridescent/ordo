54e24772075a5aa7b86609e03ccace1f08e11372
Daniel McCracken <graymcc@att.net>
2017-02-21 14:23:46 -0500

    Added join activity

---
+++ app/src/main/java/com/base512/ordo/JoinActivity.java
@@ -0,0 +1,13 @@
+package com.base512.ordo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
