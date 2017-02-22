89defc86904f6d9b7e98ddee94e9803ed63b2410
Daniel McCracken <graymcc@att.net>
2017-02-22 05:41:33 -0800

    Made create button go to lobby activity

--- app/src/main/java/com/base512/ordo/CreateActivity.java
+++ app/src/main/java/com/base512/ordo/CreateActivity.java
@@ -1,8 +1,10 @@
 package com.base512.ordo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -14,6 +16,8 @@ public class CreateActivity extends AppCompatActivity {
 
     private EditText mNumberBox;
 
+    private Button mCreateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +27,20 @@ public class CreateActivity extends AppCompatActivity {
 
     public void setupViews() {
 
+        mCreateButton = (Button) findViewById(R.id.createButton);
+
         mNumberBox = (EditText) findViewById(R.id.numberOfPictures);
 
         mAddToNumber = (ImageView) findViewById(R.id.addToNumber);
 
         mSubtractFromNumber = (ImageView) findViewById(R.id.subtractFromNumber);
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToLobby();
+            }
+        });
 
         mSubtractFromNumber.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,4 +74,10 @@ public class CreateActivity extends AppCompatActivity {
 
         mNumberBox.setText(String.valueOf(numberBoxInteger - 1));
     }
+
+    private void sendToLobby() {
+        Intent intent = new Intent(this, LobbyActivity.class);
+        startActivity(intent);
+    }
+
 }
