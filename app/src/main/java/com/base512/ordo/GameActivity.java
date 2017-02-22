0e9474ecd6495d3cf15f626ab44f6b4dd3dc7637
Daniel McCracken <graymcc@att.net>
2017-02-22 06:29:37 -0800

    Added game activity and connected it with lobby activity

---
+++ app/src/main/java/com/base512/ordo/GameActivity.java
@@ -0,0 +1,14 @@
+package com.base512.ordo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/base512/ordo/LobbyActivity.java
+++ app/src/main/java/com/base512/ordo/LobbyActivity.java
@@ -1,13 +1,37 @@
 package com.base512.ordo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_lobby);
+        setupViews();
     }
+
+    private void setupViews() {
+        mStartButton = (Button) findViewById(R.id.startGameButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToGame();
+            }
+        });
+    }
+
+    private void sendToGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
 }
