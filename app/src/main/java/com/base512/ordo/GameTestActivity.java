bf39a5d2d9506ca9a725fd41c4f61a00250e6a4e
Thomas Howe <thomas@dc0d.com>
2017-02-23 18:59:01 -0500

    Alpha commit

--- app/src/main/java/com/base512/ordo/GameTestActivity.java
+++ app/src/main/java/com/base512/ordo/GameTestActivity.java
@@ -3,12 +3,23 @@ package com.base512.ordo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ExpandedMenuView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.base512.ordo.data.Game;
+import com.base512.ordo.data.GameObject;
+import com.base512.ordo.data.source.BaseDataSource;
+import com.base512.ordo.data.source.DataModel;
 
 public class GameTestActivity extends OrdoActivity {
 
     private Button mNextButton;
+    private ImageView mGuessButton;
+    private EditText mGuessField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,43 @@ public class GameTestActivity extends OrdoActivity {
     }
 
     public void setupViews() {
-        mNextButton = (Button) findViewById(R.id.nextButton);
+        mNextButton = (Button) findViewById(R.id.gameTestResultsButton);
+        mGuessButton = (ImageView) findViewById(R.id.gameTestGuessButton);
+        mGuessField = (EditText) findViewById(R.id.gameTestGuessField);
 
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendToResults();
+            }
+        });
+
+        mGuessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                guess();
+            }
+        });
+    }
+
+    private void guess() {
+        final String guessText = mGuessField.getText().toString();
+
+        DataModel.getDataModel().getCurrentGame(new BaseDataSource.GetDataCallback<Game>() {
+            @Override
+            public void onDataLoaded(Game data) {
+                boolean isCorrect = false;
+                for(GameObject object : data.getGameObjects()) {
+                    if(object.getName().equals(guessText)) {
+                        isCorrect = true;
+                    }
+                }
+                Toast.makeText(GameTestActivity.this, String.valueOf(isCorrect), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onDataError() {
+
             }
         });
     }
